95b3610638f3cf34b1bce1ff9bf61bbe6be27335
Alessandro Scandone <alescandone@gmail.com>
2024-09-07 13:03:23 +0200

    impl unify

--- src/test/scala/typechecker/MTest.scala
+++ src/test/scala/typechecker/MTest.scala
@@ -1,6 +1,6 @@
 package typechecker
 
-import lambda.Expr
+import lambda.{Declaration, Expr, Parser}
 import org.scalatest.compatible.Assertion
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -8,14 +8,54 @@ import scala.collection.immutable.HashMap
 
 class MTest extends AnyFunSuite:
   test("type check mono variable bound in context") {
-    val i = freshIdent()
 
-    val e = Expr.Var("one")
     val ctx: Context = HashMap(
       "one" -> MonoType.concrete("Int")
     )
 
-    assert(algorithmM(ctx, e, MonoType.Var(i)).toOption.get == Substitution.fromEntries(
-      i -> MonoType.concrete("Int")
+    val t = typesOf("let x = one", ctx)
+
+    assert(t == HashMap(
+      "x" -> "Int"
     ))
+  }
+
+  test("type check abstraction") {
+    val t = typesOf("let f = \\x -> x")
+
+    assert(t == HashMap(
+      "f" -> "a -> a"
+    ))
+  }
+
+  test("type check abstraction using a fn") {
+    val ctx: Context = HashMap("iseven" -> MonoType.concrete("->", MonoType.concrete("Int"), MonoType.concrete("Bool")))
+    val t = typesOf("let f = \\x -> iseven x", ctx)
+
+    assert(t == HashMap(
+      "f" -> "Int -> Bool"
+    ))
+  }
+
+  test("type check complex abstr") {
+    val t = typesOf("let f = \\x -> \\y -> y x")
+
+    assert(t == HashMap(
+      "f" -> "a -> (a -> b) -> b"
+    ))
+  }
+
+
+  def typesOf(src: String, context: Context = HashMap.empty) = {
+    val parsed = Parser(src).get
+    parsed.foldRight(HashMap.empty)((decl, map: HashMap[String, String]) => {
+      decl match {
+        case Declaration.Let(name, expr) =>
+          val m = MonoType.Var(freshIdent())
+          val s = algorithmM(context, expr, m).toOption.get
+          val res = generalize(context, s(m))
+          map.updated(name, pprint(res))
+      }
+    })
+
   }
---
+++ src/test/scala/typechecker/Type.scala
@@ -0,0 +1,58 @@
+package typechecker
+
+import typechecker.UnifyError.{OccursCheck, TypeMismatch}
+
+import scala.collection.mutable
+
+enum Type:
+  case Var(ident: Int)
+  case Named(name: String, args: List[Type] = Nil)
+
+enum UnifyError:
+    case OccursCheck
+    case TypeMismatch
+
+
+class Unifier {
+  private val substitutions = new mutable.HashMap[Int, Type]()
+  private var nextId = 0
+
+  def freshVar(): Type = {
+    val id = nextId
+    this.nextId += 1
+    Type.Var(id)
+  }
+
+  private def resolveOnce(t: Type): Type =  t match {
+    case Type.Named(_, _) => t
+    case Type.Var(id) => substitutions.getOrElse(id, t)
+  }
+
+  // TODO resolve inner
+
+  private def unifyArgs(args: List[(Type, Type)]): Either[UnifyError, Unit] = args match {
+    case Nil => Right(())
+    case (t1, t2) :: tl =>
+      for
+        Unit <- unify(t1, t2)
+        Unit <- unifyArgs(tl)
+      yield Right(())
+  }
+
+  // TODO impl
+  private def occursIn(id: Int, t: Type) = false
+
+  def unify(t1: Type, t2: Type): Either[UnifyError, Unit] =
+    (resolveOnce(t1), resolveOnce(t2)) match {
+      case (Type.Var(id1), Type.Var(id2)) if id1 == id2 => Right(())
+      case (Type.Named(name1, _), Type.Named(name2, _)) if name1 != name2 => Left(TypeMismatch)
+      case (Type.Var(id), t@Type.Named(_, _)) if occursIn(id, t) => Left(OccursCheck)
+      case (Type.Named(_, args1), Type.Named(_, args2)) => unifyArgs(args1 zip args2)
+      case (Type.Var(id), _) =>
+        substitutions.put(id, t2)
+        Right(())
+      case (Type.Named(_, _), Type.Var(_)) => unify(t2, t1)
+    }
+}
+
+
