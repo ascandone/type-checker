85e3f1bc8eee17ea305d6558097f5d4a7a672127
Alessandro Scandone <alescandone@gmail.com>
2024-09-09 11:30:36 +0200

    cleaned up code

--- src/test/scala/typechecker/Type.scala
+++ src/test/scala/typechecker/Type.scala
@@ -1,7 +1,7 @@
 package typechecker
 
 import typechecker.UnifyError.{OccursCheck, TypeMismatch}
-
+import typechecker.Type.{Named, Var}
 import scala.collection.mutable
 
 enum Type:
@@ -9,8 +9,8 @@ enum Type:
   case Named(name: String, args: List[Type] = Nil)
 
 enum UnifyError:
-    case OccursCheck
-    case TypeMismatch
+  case OccursCheck
+  case TypeMismatch
 
 
 class Unifier {
@@ -43,17 +43,16 @@ class Unifier {
   private def occursIn(id: Int, t: Type) = false
 
   def unify(t1: Type, t2: Type): Either[UnifyError, Unit] =
-    (resolveOnce(t1), resolveOnce(t2)) match {
-      case (Type.Named(name1, _), Type.Named(name2, _)) if name1 != name2 => Left(TypeMismatch)
-      case (Type.Named(_, args1), Type.Named(_, args2)) => unifyArgs(args1 zip args2)
+    (resolveOnce(t1), resolveOnce(t2)) match
+      case (Named(name1, _), Type.Named(name2, _)) if name1 != name2 => Left(TypeMismatch)
+      case (Named(_, args1), Named(_, args2)) => unifyArgs(args1 zip args2)
 
-      case (Type.Var(id1), Type.Var(id2)) if id1 == id2 => Right(())
-      case (Type.Var(id), t@Type.Named(_, _)) if occursIn(id, t) => Left(OccursCheck)
-      case (Type.Var(id), _) =>
+      case (Var(id1), Var(id2)) if id1 == id2 => Right(())
+      case (Var(id), t@Named(_, _)) if occursIn(id, t) => Left(OccursCheck)
+      case (Var(id), _) =>
         substitutions.put(id, t2)
         Right(())
-      case (Type.Named(_, _), Type.Var(_)) => unify(t2, t1)
-    }
+      case (Named(_, _), Var(_)) => unify(t2, t1)
 }
 
 
